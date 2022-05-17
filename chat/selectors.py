faa1d9cf7ddf95711a59c41d7a7e57876123a8aa
latikagandhi <latika.gandhi@techstriker.com>
2022-05-17 13:34:51 +0530

    pagination added in getMessage emit.

--- chat/selectors.py
+++ chat/selectors.py
@@ -26,6 +26,9 @@ def get_rooms(input_data, user_id):
 
 def get_messages(input_data):
     # import pdb;pdb.set_trace()
+    page = input_data.get('page', 1)
+    page_size = input_data.get('page_size', 10)
+    # room = ChatRoom.objects(id=input_data['room']).get().to_json()
     room = ChatRoom.objects(id=input_data['room']).get().to_json()
     if not room['is_group']:
         if room['participants']:
@@ -37,9 +40,30 @@ def get_messages(input_data):
                 room['image'] = room['participants'][0]['profile_image']
     # import pdb;
     # pdb.set_trace()
-    messages = Message.objects(recipients__room=input_data['room'])
+    # messages = Message.objects(recipients__room=input_data['room'])
+    
+
+
     unread_count = Message.objects(recipients__room=room['id'], recipients__is_read=False,
                                    recipients__recipient__user_id=input_data['user_id']).count()
+    
+    if unread_count > 0:
+        page_size = unread_count+20
+    
+    messages_pagination = Message.objects(recipients__room=input_data['room']).paginate(page=page, per_page=page_size)
+    list_of_items = messages_pagination.items
+
+
     return generate_response(
-        data={'room': room, 'messages': [message.to_json() for message in messages], 'unread_count': unread_count},
+        data={
+            'room': room, 
+            'messages': [message.to_json() for message in list_of_items], 
+            'unread_count': unread_count,
+            'paginate': {
+                "page" : messages_pagination.page,
+                "page_size" : messages_pagination.per_page,
+                "total_pages" : messages_pagination.pages,
+                "total_number_of_items" : messages_pagination.total
+            },
+            },
         status=HTTP_200_OK)
